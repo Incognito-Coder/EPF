36cd201767a2aea59f58fe3e429d3de9c449d432
Incognito-Coder <mr.alireza.ahmand@gmail.com>
2023-04-07 11:37:04 +0330

    first commit

---
+++ iptables.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ip=$1
+ports=$2
+proto=$3
+
+usage(){
+    echo -e "Usage: iptables.sh 1 2 3\n1: Destination IP\n2: Desired Ports | Example Array of ports: 443,80,2083\n3: Protocol TCP/UDP lowercase."
+}
+
+modify(){
+    echo -e "Easy Port Forwarder By Incognito Coder\nGithub Page: https://github.com/Incognito-Coder"
+    publicIP=$(hostname -I | awk '{print $1}')
+    interface=$(route | grep '^default' | grep -o '[^ ]*$')
+    echo "Enabling IP Forwarding"
+    sysctl net.ipv4.ip_forward=1 > /dev/null 2>&1
+    if iptables --table nat --list | grep -q "ssh"; then
+        echo "No need to forward SSH Port,Already Exist!"
+    else
+        echo "Forwarding SSH Port to $publicIP"
+        iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination $publicIP
+    fi
+    for i in $(echo $ports | tr "," "\n")
+    do
+        echo "Moving Port $i to $ip:$i"
+        iptables -t nat -A PREROUTING -p $proto --dport $i -j DNAT --to-destination $ip
+    done
+    echo "Finalizing Changes in IP Tables."
+    iptables -t nat -A POSTROUTING -j MASQUERADE -o $interface
+}
+
+flush(){
+    echo "Stopping IPv4 firewall and allowing everyone..."
+    ipt="/sbin/iptables"
+    [ ! -x "$ipt" ] && { echo "$0: \"${ipt}\" command not found."; exit 1; }
+    $ipt -P INPUT ACCEPT
+    $ipt -P FORWARD ACCEPT
+    $ipt -P OUTPUT ACCEPT
+    $ipt -F
+    $ipt -X
+    $ipt -t nat -F
+    $ipt -t nat -X
+    $ipt -t mangle -F
+    $ipt -t mangle -X
+    $ipt -t raw -F
+    $ipt -t raw -X
+}
+
+if [[ $# -eq 0 ]] ; then
+    usage
+    exit 0
+else
+    if [[ $1 == "flush" ]]; then
+        flush
+        exit 0
+    else
+        modify
+    fi
+fi
